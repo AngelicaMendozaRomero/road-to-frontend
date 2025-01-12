70df44b5aa233355a71770decec0ec743fecdf9e
AngelicaMendozaRomero <amr.angelica16@gmail.com>
2025-01-12 00:35:01 -0600

    proyecto generado por mi
    genere mi proyecto con
    npm create vite@latest
    corri mi proyecto con
    npm run dev que esta agregado en el package.json

---
+++ sala-de-cine/src/main.js
@@ -0,0 +1,8 @@
+import './style.css';
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h3 class="titulo" >Mi primer proyecto generado por mi</h3>
+    <p id="descripcion">maestro jesus</p>
+  </div>
+`;
